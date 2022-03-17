db0981afc56dfc3798f8203dfb1d204f8a718947
Khristiam Zuluaga <khristiam.paiva@mercadolibre.com.co>
2022-03-16 22:56:32 -0500

    Version final

---
+++ src/main/java/com/kzuluaga/workshop_git/MainController.java
@@ -0,0 +1,21 @@
+package com.kzuluaga.workshop_git;
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/user")
+public class MainController {
+
+    @GetMapping
+    public ResponseEntity<String> getMEssage(){
+
+        HttpHeaders headers
+
+        return new ResponseEntity("Success", headers, HttpStatus.OK);
+    }
+}
