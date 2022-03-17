745249a22065974bec890abce764473d946e60ad
Khristiam Zuluaga <khristiam.paiva@mercadolibre.com.co>
2022-03-16 23:13:05 -0500

    Nueva funcion change

--- src/main/java/com/kzuluaga/workshop_git/MainController.java
+++ src/main/java/com/kzuluaga/workshop_git/MainController.java
@@ -4,6 +4,7 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,5 +20,14 @@ public class MainController {
         return new ResponseEntity("Successfully", headers, HttpStatus.OK);
     }
 
-    public ResponseEntity<String> createMessage()
+    @PostMapping
+    public ResponseEntity<String> createMessage(){
+
+        HttpHeaders headers = new HttpHeaders();
+        return new ResponseEntity("Successfully", headers, HttpStatus.OK);
+    }
+
+    public ResponseEntity<String> changeMessage(){
+
+    }
 }
