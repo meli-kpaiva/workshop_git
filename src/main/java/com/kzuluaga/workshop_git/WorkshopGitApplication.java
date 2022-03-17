9e3b8e51e0994a359eb4390843208a40c3b6f7d9
Khristiam Zuluaga <khristiam.paiva@mercadolibre.com.co>
2022-03-16 22:47:39 -0500

    Versio Inicial

---
+++ src/main/java/com/kzuluaga/workshop_git/WorkshopGitApplication.java
@@ -0,0 +1,13 @@
+package com.kzuluaga.workshop_git;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkshopGitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkshopGitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kzuluaga/workshop_git/WorkshopGitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kzuluaga.workshop_git;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopGitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
