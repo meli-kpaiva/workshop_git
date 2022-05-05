51b5a7a586e743ded3da555d26f0876da43d95db
Khristiam Zuluaga <khristiam.paiva@mercadolibre.com.co>
2022-05-05 17:21:49 -0500

    Se agrega delete

--- src/main/java/com/kzuluaga/workshop_git/MainController.java
+++ src/main/java/com/kzuluaga/workshop_git/MainController.java
@@ -3,6 +3,7 @@ package com.kzuluaga.workshop_git;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -29,6 +30,12 @@ public class MainController {
 
     @PostMapping
     public ResponseEntity<String> changeMessageByID(){
+        HttpHeaders headers = new HttpHeaders();
+        return new ResponseEntity("Successfully", headers, HttpStatus.OK);
+    }
+
+    @DeleteMapping
+    public ResponseEntity<String> delete(){
         HttpHeaders headers = new HttpHeaders();
         return new ResponseEntity("Successfully", headers, HttpStatus.OK);
     }
