cc6e070f8619f642fce5129c89f562ca26d9ef66
Khristiam Zuluaga <khristiam.paiva@mercadolibre.com.co>
2022-05-05 17:22:30 -0500

    Agrega patch

--- src/main/java/com/kzuluaga/workshop_git/MainController.java
+++ src/main/java/com/kzuluaga/workshop_git/MainController.java
@@ -5,6 +5,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -36,6 +37,12 @@ public class MainController {
 
     @DeleteMapping
     public ResponseEntity<String> delete(){
+        HttpHeaders headers = new HttpHeaders();
+        return new ResponseEntity("Successfully", headers, HttpStatus.OK);
+    }
+
+    @PatchMapping
+    public ResponseEntity<String> edit(){
         HttpHeaders headers = new HttpHeaders();
         return new ResponseEntity("Successfully", headers, HttpStatus.OK);
     }
